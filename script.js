63d4bbdb33d27d3bf83807868958900cf64e0652
Ahmed Aldofani <Ahmeddou55@yahoo.com>
2023-07-28 21:27:09 +0100

    Visual features

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+// Get the theme toggle button and the theme style link element
+const themeToggle = document.getElementById('theme-toggle');
+const themeStyle = document.getElementById('theme-style');
+
+// Check for saved user preference (light/dark mode)
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.body.classList.add(currentTheme);
+}
+
+// Toggle between light and dark mode when the button is clicked
+themeToggle.addEventListener('click', () => {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+    localStorage.setItem('theme', 'light-mode');
+    // Style the h1 element when in light mode
+    const h1Elements = document.getElementsByTagName("h1");
+    for (const h1 of h1Elements) {
+      h1.style.color = 'black';
+    }
+    
+    
+    
+  } else {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark-mode');
+    
+    // Style the h1 element when in light mode
+    const h1Elements = document.getElementsByTagName("h1");
+    for (const h1 of h1Elements) {
+    h1.style.color = 'white';
+}
+     
+  }
+});
+
+
+
 // Get the starting date when you began coding
 const startDate = new Date('2022-10-06'); // Replace with your actual start date
 
@@ -23,26 +63,5 @@ function updateCounter() {
 // Start updating the counter
 updateCounter();
 
-// Get the theme toggle button and the theme style link element
-const themeToggle = document.getElementById('theme-toggle');
-const themeStyle = document.getElementById('theme-style');
 
-// Check for saved user preference (light/dark mode)
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-  document.body.classList.add(currentTheme);
-}
-
-// Toggle between light and dark mode when the button is clicked
-themeToggle.addEventListener('click', () => {
-  if (document.body.classList.contains('dark-mode')) {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode');
-    localStorage.setItem('theme', 'light-mode');
-  } else {
-    document.body.classList.remove('light-mode');
-    document.body.classList.add('dark-mode');
-    localStorage.setItem('theme', 'dark-mode');
-  }
-});
 
